b703c1ca5f0f758f8384a5294bb38f2a790bdc03
quizzzzzy <112960896+quizzzzzy@users.noreply.github.com>
2022-09-09 17:02:28 +0300

    Add files via upload

---
+++ testy.py
@@ -0,0 +1,7 @@
+user_money = float(input("сколько у вас рублей"))
+dinar = 196.23
+rial = 0.0014
+us_dollar = 60.63
+print("У вас", round(user_money/ dinar, 2), "кувейтских динар")
+print("У вас", round(user_money/ rial , 2), "иранских риалов")
+print("У вас", round(user_money/us_dollar, 2), "долларов США")
