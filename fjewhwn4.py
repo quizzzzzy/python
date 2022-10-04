59061e1cf9746cb2a54dc8378ce8d01a3ab9c823
quizzzzzy <112960896+quizzzzzy@users.noreply.github.com>
2022-10-04 17:22:46 +0300

    Add files via upload

---
+++ fjewhwn4.py
@@ -0,0 +1,18 @@
+import random
+print("начало боя")
+name = input("введите имя героя")
+print(f"{name} решил истребить разбойников")
+print("нажмите клавишу Enter чтобы начать атаковать")
+user_health = 100
+OGbuda_health = 100
+atack = random.randint(50,100)
+if:
+    print("вы снесли разбойнику: ")
+    int(input())
+
+
+
+
+
+
+
